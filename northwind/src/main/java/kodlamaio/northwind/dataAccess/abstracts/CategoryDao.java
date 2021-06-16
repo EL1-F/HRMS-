d4eff7c6a410909ed017a590b1151bc891499081
EL1-F <77543411+EL1-F@users.noreply.github.com>
2021-06-17 00:41:55 +0300

    Delete northwind directory

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.northwind.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.northwind.entities.concretes.Category;
-
-public interface CategoryDao extends JpaRepository<Category, Integer>{
-
-}
